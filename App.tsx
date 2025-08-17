b1e87b5c3cca33d4818d9877b7a81c3b6d30c601
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-08-17 19:11:07 +0530

    Learning haptic-feedback and device apis

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Form from './src/components/Form'
+import RollTheDice from './src/components/RollTheDice'
 
 const App = () => {
   return (
     <View className='dark:bg-neutral-800'>
-      <Form />
+      <RollTheDice />
     </View>
   )
 }
---
+++ src/components/RollTheDice.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class RollTheDice extends Component {
+    render() {
+        return (
+            <View>
+                <Text>RollTheDice</Text>
+            </View>
+        )
+    }
+}
+
+export default RollTheDice
