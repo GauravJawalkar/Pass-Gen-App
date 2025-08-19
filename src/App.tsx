6b7dadbcd231d90f7c217a7b83788480c7cb8685
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-08-19 23:10:11 +0530

    password generator and dice done also learned about the react-haptic feedbacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import Form from './components/Form'
-import RollTheDice from './components/RollTheDice'
 
 const App = () => {
   return (
     <View className='dark:bg-neutral-800'>
-      <RollTheDice />
+      <Form />
     </View>
   )
 }
